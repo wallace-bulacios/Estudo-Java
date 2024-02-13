a983bbc139d57df378dfe68d7f91253a149ed364
Wallace V. I. Bulacios <bulacios.dev@gmail.com>
2024-02-13 10:48:57 -0300

    Primeiro commit e atualização

---
+++ workspace/primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,11 @@
+package cursojava.classes;
+
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	
+}
---
+++ workspace/primeiro-programa-java/src/cursojava/exceutavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.exceutavel;
+
+public class PrimeiraClasseJava {
+		
+	/* Método auto executavel em java */
+	public static void main(String[] args) {
+		
+		
+	}
+}
